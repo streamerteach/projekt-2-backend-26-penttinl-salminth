6da45ef1b58f39ed2727a87ee8312eb8bf9df239
salminth <salminth@arcada.fi>
2026-01-14 20:40:58 +0200

    första committen

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Backend programmering</title>
+</head>
+<body>
+    <h1>Dating site</h1>
+
+    <?php
+        print(1 + 2);
+        print($_SERVER['SERVER_ADDR']);
+
+        print($_SERVER['SERVER_SOFTWARE']);
+
+        print($_SERVER['REMOTE_USER']);
+
+        //phpinfo();
+         
+    ?>
+
+</body>
+</html>
