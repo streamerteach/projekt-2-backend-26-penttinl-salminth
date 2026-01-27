a957287aa4f64096bbe29f25510e0e644f412d85
salminth <salminth@arcada.fi>
2026-01-27 12:53:04 +0200

    Nav

--- index.php
+++ index.php
@@ -6,7 +6,19 @@
     <title>Projekt1</title>
     <link rel="stylesheet" href="./style.css">
 </head>
-<body>
-    
-</body>
+    <body>
+        <div id="container">
+            <header>
+                <nav>
+                    <ul>
+                        <a href=><li>Home</li></a>
+                        <a href=><li>Login</li></a>
+                        <a href=><li>Webbrapport</li></a>
+                    </ul>
+                </nav>
+            </headrer>
+            
+        
+        
+    </body>
 </html>
