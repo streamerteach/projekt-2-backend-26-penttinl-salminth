9eef492c3bfcf03696e5c0280fd8480be3212e18
salminth <salminth@arcada.fi>
2026-01-27 13:44:25 +0200

    entiiä

--- essentials/nav.php
+++ essentials/nav.php
@@ -1,15 +1,10 @@
-   <header>
-        <div id="logo"> Healthy Dating </div>
-            <nav>
-                <ul>
-                <a href="../home/"><li>Home</li></a>
-                <?php
-                    if (! empty($_SESSION['username'])) {
-                        print('<a href="../profile/"><li>profile</li></a>');
-                    } else {
-                        print('<a href="../login/"><li>login</li></a>');
-                    }
-                ?>
-                </ul>
-            </nav>
-    </header>
+
+<header>
+    <nav>
+        <ul>
+            <a href=><li>Home</li></a>
+            <a href=><li>Login</li></a>
+            <a href=><li>Webbrapport</li></a>
+        </ul>
+    </nav>
+</headrer>
