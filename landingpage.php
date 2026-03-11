1b97ae06b766e0d6118aa6937aee6bf8194e706a
salminth <salminth@arcada.fi>
2026-03-11 13:47:52 +0200

    Projekt2 titel

--- landingpage.php
+++ landingpage.php
@@ -15,7 +15,7 @@ $firstVisit = $firstVisit ?? null;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projekt1</title>
+    <title>Projekt2</title>
     <link rel="stylesheet" href="./style.css">
 </head>
     <body>
